bd182c7a5147e373d6efa0259f1ce9699950f4f7
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-07-20 22:02:51 +0700

    edit view voter

--- application/modules2/dashboard/views/role/voter.php
+++ application/modules2/dashboard/views/role/voter.php
@@ -12,26 +12,38 @@
 				<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
 					<thead>
 						<tr>
-							<th>Nama Lengkap</th>
-							<th>Nama Panggilan</th>
 							<th>NRP</th>
-							<th>KTP</th>						
+							<th>Nama Lengkap</th>
+							<th>Penghasilan Orang Tua</th>
+							<th>UKT</th>
+							<th>IPK</th>
+							<th>Aksi</th>
+							<th>Vote</th>						
                         </tr>
 					</thead>
 					<tbody>
 						<?php foreach ($pendaftar as $pendaftar): ?>
 							<tr>
 								<td>
-									<?php echo $pendaftar->nama_lengkap ?>
-								</td>
-								<td>
-									<?php echo $pendaftar->nama_panggilan ?>
-								</td>
-								<td>
 									<?php echo $pendaftar->nrp ?>
 								</td>
 								<td>
-									<?php echo $pendaftar->ktp ?>
+									<?php echo $pendaftar->nama_lengkap ?>
+								</td>
+								<td>
+									<?php echo $pendaftar->penghasilan_ortu ?>
+								</td>
+								<td>
+									<?php echo $pendaftar->ukt ?>
+								</td>
+								<td>
+									<?php echo $pendaftar->ipk ?>
+								</td>
+								<td>
+									<a href="<?php echo site_url('dashboard') ?>" role="button" class="btn btn-primary">Detail</a>
+								</td>
+								<td>
+									<input type="checkbox" name="pendaftar_id" value="<?php echo $pendaftar->pendaftar_id; ?>">
 								</td>
 							</tr>
 						<?php endforeach; ?>
