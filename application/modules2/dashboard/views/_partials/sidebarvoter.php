5a387cbbee1c7bf0d87a708d01d88bcf18442075
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-07-23 22:00:39 +0700

    update voting

--- application/modules2/dashboard/views/_partials/sidebarvoter.php
+++ application/modules2/dashboard/views/_partials/sidebarvoter.php
@@ -2,7 +2,7 @@
 <ul class="sidebar navbar-nav">
     <li class="nav-item <?php echo $this->uri->segment(2) == '' ? 'active': '' ?>">
         <a class="nav-link" href="<?php echo site_url('dashboard') ?>">
-            <i class="fas fa-fw fa-bullhorn"></i>
+            <i class="fas fa-fw fa-user-plus"></i>
             <span>Pemberian Suara</span>
         </a>
     </li>
