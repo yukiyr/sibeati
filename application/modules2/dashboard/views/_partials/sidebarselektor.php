a1f318b70715d2ddcf946a786cac4e9463655dd3
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-07-24 21:01:46 +0700

    sidebar crud berita dan beasiswa

--- application/modules2/dashboard/views/_partials/sidebarselektor.php
+++ application/modules2/dashboard/views/_partials/sidebarselektor.php
@@ -7,14 +7,14 @@
         </a>
     </li>
 
-    <li class="nav-item <?php echo $this->uri->segment(1) == 'berita' ? 'active': '' ?>">
-        <a class="nav-link" href="<?php echo site_url('berita/list') ?>">
+    <li class="nav-item <?php echo $this->uri->segment(2) == 'berita' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('kelolaberita/berita') ?>">
             <i class="fas fa-fw fa-bullhorn"></i>
             <span>Kelola Berita</span></a>
     </li>
 
-    <li class="nav-item <?php echo $this->uri->segment(1) == 'beasiswa' ? 'active': '' ?>">
-        <a class="nav-link" href="<?php echo site_url('beasiswa/list') ?>">
+    <li class="nav-item <?php echo $this->uri->segment(2) == 'beasiswa' ? 'active': '' ?>">
+        <a class="nav-link" href="<?php echo site_url('kelolabeasiswa/beasiswa') ?>">
             <i class="fas fa-fw fa-graduation-cap"></i>
             <span>Kelola Beasiswa</span></a>
     </li>
