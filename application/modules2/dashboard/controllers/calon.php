fb0d9fdf29baf5212416a937a38562e76a0ba6da
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-07-25 10:06:37 +0700

    alert melebihi jumlah kuota vote/beasiswa

--- application/modules2/dashboard/controllers/calon.php
+++ application/modules2/dashboard/controllers/calon.php
@@ -16,17 +16,41 @@ class Calon extends CI_Controller
     public function add_calon()
     {
         $this->load->model('calon_model');
-        $pendaftar_id = $this->input->post('pendaftar_id');
+        $this->load->model('pendaftar_model');
 
-        for($i=0; $i < sizeof($pendaftar_id); $i++)
-        {
-            $result = $this->calon_model->save_calon($pendaftar_id[$i]);
+        $pendaftar_id = $this->input->post('pendaftar_id');
+        $kuota = $this->calon_model->getKuota();
+
+        $limit = array();
+        $jumlah = $this->pendaftar_model->countVote();
+        foreach($jumlah as $jumlah) {
+            if ($jumlah->user_id == $this->session->userdata('user_id'))
+            {
+                $limit[] = $jumlah->pendaftar_id;
+            }
         }
 
-        if($result != 0){
-            //pemberian suara berhasil
+        $tambah = count($limit) + sizeof($pendaftar_id);
+
+        if(sizeof($pendaftar_id) <= $kuota || $tambah <= $kuota)
+        {
+            for($i=0; $i < sizeof($pendaftar_id); $i++)
+            {
+                $result = $this->calon_model->save_calon($pendaftar_id[$i]);
+            }
+
+            if($result != 0){
+                //pemberian suara berhasil
+                echo "<script>
+                alert('Selamat, pemberian suara sukses');
+                window.location='".site_url('dashboard')."';
+                </script>";
+            }
+        }
+        else
+        {
             echo "<script>
-            alert('Selamat, pemberian suara sukses');
+            alert('Maaf, Anda melebihi kuota vote');
             window.location='".site_url('dashboard')."';
             </script>";
         }
